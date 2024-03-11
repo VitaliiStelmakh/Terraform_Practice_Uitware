fd9995c4e4452ee2957a1767c781745d997177c8
Віталій Стельмах <vitalistelmakh30@gmail.com>
2024-03-11 22:18:23 +0200

    delete default value

--- variables.tf
+++ variables.tf
@@ -16,17 +16,14 @@ variable "client_secret" {
 
 variable "tenant_id" {
     description = "The Azure Tenant ID"
-    default = "7631cd62-5187-4e15-8b8e-ef653e366e7a"
   type = string
 }
 
 variable "sql_server_login" {
     description = "The login to SQL Server"
-    default = "vitaliistelmakh"
   type = string
 }
 variable "sql_server_password" {
     description = "The password to SQL Server"
-    default = "Qwerty123123"
   type = string
 }
