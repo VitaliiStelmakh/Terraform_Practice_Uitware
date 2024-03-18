ee6b34a29039dd511e552ba8770ca4f86021f099
Віталій Стельмах <vitalistelmakh30@gmail.com>
2024-03-18 14:49:07 +0200

    format text

--- variables.tf
+++ variables.tf
@@ -1,12 +1,13 @@
-
 variable "subscription_id" {
     description = "The Azure Subscription ID"
   type = string
 }
+
 variable "client_id" {
     description = "The Azure Client ID"
   type = string
 }
+
 variable "client_secret" {
     description = "The Azure Client Secret"
   type = string
