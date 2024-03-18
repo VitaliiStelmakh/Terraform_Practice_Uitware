acab345b2f9d6164e8e228e89198755ed5747150
vitalikstelmah60 <51053522+vitalikstelmah60@users.noreply.github.com>
2024-03-18 15:30:31 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,10 +3,12 @@ variable "subscription_id" {
     description = "The Azure Subscription ID"
   type = string
 }
+
 variable "client_id" {
     description = "The Azure Client ID"
   type = string
 }
+
 variable "client_secret" {
     description = "The Azure Client Secret"
   type = string
